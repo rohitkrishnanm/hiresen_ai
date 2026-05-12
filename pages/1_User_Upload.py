6f9a053717d7e2d0ca89e214018104563714ba92
Rohit Krishnan <62697933+rohitkrishnanm@users.noreply.github.com>
2026-05-12 13:37:04 +0530

    Update 1_User_Upload.py

--- pages/1_User_Upload.py
+++ pages/1_User_Upload.py
@@ -1,6 +1,6 @@
-import streamlit as st
+##import streamlit as st
 st.set_page_config(page_title="Upload Resume", page_icon="📄", layout="wide")
-import uuid
+##import uuid
 import datetime
 
 # Import core modules
